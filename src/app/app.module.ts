12afe13e96c6bc97ea2671091ef93ba7c70422f1
Maxim Chikhanov <maxim1999chikhanov@gmail.com>
2022-03-29 23:54:17 +0300

    6.1 - errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { DataModule } from "./data/data.module";
+
+import { BoldDirective } from "./bold.directive";
+import { WhileDirective } from "./while.directive";
+
 import { AppComponent } from "./app.component";
 import { ChildComponent } from "./child.component";
 import { PreChildComponent } from "./prechild.component";
 import { SimpleComponent } from "./simple.component";
-import { DataModule } from "./data/data.module";
-import { BoldDirective } from "./bold.directive";
-import { WhileDirective } from "./while.directive";
 import { NewComponent } from "./new.component";
 import { OneElseComp } from "./else.component";
 import { TwoComponent } from "./two.component";
 import { FormComponent } from "./form.component";
+import { UserComponent } from "./user.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, DataModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    DataModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   declarations: [
     AppComponent,
     ChildComponent,
@@ -26,7 +37,9 @@ import { FormComponent } from "./form.component";
     OneElseComp,
     TwoComponent,
     FormComponent,
+    UserComponent,
   ],
+  exports: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
